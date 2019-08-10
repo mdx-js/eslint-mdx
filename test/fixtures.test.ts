1b31ac04b3e833454d43f738dcc281737eec01bc
JounQin <admin@1stg.me>
2019-08-10 18:17:40 +0800

    test: e2e test based on jest snapshots, close #20

---
+++ test/fixtures.test.ts
@@ -0,0 +1,18 @@
+import { basename } from 'path'
+
+import { CLIEngine } from 'eslint'
+
+export const cli = new CLIEngine({
+  ignore: false,
+  fix: true,
+})
+
+describe('fixtures', () => {
+  it('should match all snapshots', () => {
+    cli
+      .executeOnFiles(['test/fixtures/*.mdx'])
+      .results.forEach(({ filePath, output }) =>
+        expect(output).toMatchSnapshot(basename(filePath)),
+      )
+  })
+})
