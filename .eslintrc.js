d557bda5a23c77c0d41e16ed137dd826fefbf5fb
JounQin <admin@1stg.me>
2019-12-10 23:24:30 +0800

    chore: bump (dev)Dependencies, add FUNDING.yml (#113)

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ tsNode.register({
 
 module.exports = {
   root: true,
-  extends: ['@1stg/eslint-config/recommended'],
+  extends: ['@1stg'],
   rules: {
     '@typescript-eslint/no-unnecessary-condition': 0,
     '@typescript-eslint/unbound-method': 0, // See https://github.com/typescript-eslint/typescript-eslint/issues/636
