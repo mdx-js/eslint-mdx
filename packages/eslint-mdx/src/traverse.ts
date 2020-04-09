1adc171a3b436c22cd33e64dfebbbfdc31fcdfaf
JounQin <admin@1stg.me>
2020-04-09 22:05:53 +0800

    fix: self closing tag should not be combined - close #158 (#196)
    * fix: self closing tag should not be combined - close #158
    
    * fix: open tag index could be -1

--- packages/eslint-mdx/src/traverse.ts
+++ packages/eslint-mdx/src/traverse.ts
@@ -76,7 +76,18 @@ export class Traverse {
           }
 
           if (!offset) {
-            acc.push(this.combineLeftJsxNodes(jsxNodes))
+            // fix #158
+            const firstOpenTagIndex = jsxNodes.findIndex(node =>
+              isOpenTag(node.value as string),
+            )
+            if (firstOpenTagIndex === -1) {
+              acc.push(...jsxNodes)
+            } else {
+              acc.push(...jsxNodes.slice(0, firstOpenTagIndex))
+              acc.push(
+                this.combineLeftJsxNodes(jsxNodes.slice(firstOpenTagIndex)),
+              )
+            }
             jsxNodes.length = 0
           }
         }
