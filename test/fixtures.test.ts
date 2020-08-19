ff65faacf823b9927f73249c65d30e9f77ed7222
nd0ut <nd0ut.me@gmail.com>
2020-08-19 16:46:22 +0300

    test(eslint-mdx): add test for parsing many comments

--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -14,4 +14,12 @@ describe('fixtures', () => {
       expect(output || source).toMatchSnapshot(basename(filePath)),
     )
   })
+
+  it('should parse many comments', async () => {
+    const results = await cli.lintFiles(['test/fixtures/many-comments.mdx'])
+
+    return results.forEach(({ errorCount }) => {
+      expect(errorCount).toBe(0)
+    })
+  })
 })
