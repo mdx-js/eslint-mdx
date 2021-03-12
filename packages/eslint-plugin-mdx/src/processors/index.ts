5052eb8eb638242c940ce4f48a1245ef07dd6fe4
JounQin <admin@1stg.me>
2021-03-12 10:54:37 +0800

    feat: use processor to display remark reports directly (#281)

---
+++ packages/eslint-plugin-mdx/src/processors/index.ts
@@ -0,0 +1,4 @@
+import { remark } from './remark'
+
+// related to https://github.com/benmosher/eslint-plugin-import/issues/2002
+export const processors = { remark }
--- packages/eslint-plugin-mdx/src/rules/types.ts
+++ packages/eslint-plugin-mdx/src/rules/types.ts
@@ -1,3 +1,4 @@
+import { Linter } from 'eslint'
 // eslint-disable-next-line node/no-extraneous-import
 import { ExpressionStatement, Node } from 'estree'
 import { Attacher } from 'unified'
@@ -17,4 +18,11 @@ export type RemarkPlugin = string | Attacher
 export interface RemarkConfig {
   settings: Record<string, string>
   plugins: Array<RemarkPlugin | [RemarkPlugin, ...unknown[]]>
+}
+
+export interface RemarkLintMessage {
+  reason: string
+  source: string
+  ruleId: string
+  severity: Linter.Severity
 }
