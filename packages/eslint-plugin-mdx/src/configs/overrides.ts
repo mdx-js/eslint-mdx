49740939da02242f771aecdc4a6beaa48602023c
Sam Chen <chenxsan@gmail.com>
2021-03-20 13:43:24 +0800

    fix: clean up `rebass` which is not bundled with mdx (#295)
    If you’re using `rebass` or other library as global components of mdx, you can use the helper `getGlobals` which is exported from `eslint-plugin-mdx` to simplify the usage.

--- packages/eslint-plugin-mdx/src/configs/overrides.ts
+++ packages/eslint-plugin-mdx/src/configs/overrides.ts
@@ -1,25 +1,12 @@
 import type { Linter } from 'eslint'
-import type Rebass from 'rebass'
 
 import { base } from './base'
-import { getGlobals } from './helpers'
-
-let rebass: typeof Rebass | string[]
-
-try {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
-  rebass = require('rebass') as typeof rebass
-} catch {
-  // `rebass`(or `reflexbox` actually) requires `react` as peerDependency, but not all projects using `mdx` are `React` based, so we fallback to hardcoded `rebass` Components here
-  /* istanbul ignore next */
-  rebass = ['Box', 'Flex', 'Text', 'Heading', 'Link', 'Button', 'Image', 'Card']
-}
 
 export const overrides: Linter.Config = {
   ...base,
-  globals: getGlobals(rebass, {
-    React: false,
-  }),
+  globals: {
+    React: 'readonly',
+  },
   rules: {
     'lines-between-class-members': 0, // See https://github.com/mdx-js/mdx/issues/195
     'react/jsx-no-undef': [
