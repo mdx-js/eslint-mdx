a74c7c19e5fb8c9ba4126da99a7725f3b1aedc52
JounQin <admin@1stg.me>
2021-04-07 16:56:32 +0000

    chore: use @types/eslint-plugin-markdown

--- packages/eslint-plugin-mdx/typings.d.ts
+++ packages/eslint-plugin-mdx/typings.d.ts
@@ -1,11 +1,3 @@
-declare module 'eslint-plugin-markdown' {
-  import type { Linter } from 'eslint'
-
-  export const processors: {
-    markdown: Linter.Processor<Linter.ProcessorFile>
-  }
-}
-
 declare module 'remark-mdx' {
   import type * as unified from 'unified'
   const mdx: unified.Attacher
