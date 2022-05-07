b6f299c5a59ebd63336b13dbdf715b2b29dbcaa3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-05-07 13:53:52 +0200

    lint fix

--- packages/eslint-plugin-mdx/src/processors/remark.ts
+++ packages/eslint-plugin-mdx/src/processors/remark.ts
@@ -30,27 +30,29 @@ export const remark: Linter.Processor = {
     return [
       ...mdxMessages,
       ...processors.markdown.postprocess(markdownMessages, filename),
-    ].sort((a, b) => a.line - b.line).map(lintMessage => {
-      const {
-        message,
-        ruleId: eslintRuleId,
-        severity: eslintSeverity,
-      } = lintMessage
+    ]
+      .sort((a, b) => a.line - b.line)
+      .map(lintMessage => {
+        const {
+          message,
+          ruleId: eslintRuleId,
+          severity: eslintSeverity,
+        } = lintMessage
 
-      if (eslintRuleId !== 'mdx/remark') {
-        return lintMessage
-      }
+        if (eslintRuleId !== 'mdx/remark') {
+          return lintMessage
+        }
 
-      const { source, ruleId, reason, severity } = JSON.parse(
-        message,
-      ) as RemarkLintMessage
+        const { source, ruleId, reason, severity } = JSON.parse(
+          message,
+        ) as RemarkLintMessage
 
-      return {
-        ...lintMessage,
-        ruleId: `${source}-${ruleId}`,
-        message: reason,
-        severity: Math.max(eslintSeverity, severity) as Linter.Severity,
-      }
-    })
+        return {
+          ...lintMessage,
+          ruleId: `${source}-${ruleId}`,
+          message: reason,
+          severity: Math.max(eslintSeverity, severity) as Linter.Severity,
+        }
+      })
   },
 }
