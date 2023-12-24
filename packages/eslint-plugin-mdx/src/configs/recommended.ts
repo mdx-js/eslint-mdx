58f469f981987c1936e88b4a46758038cc2b0dec
JounQin <admin@1stg.me>
2023-12-24 15:23:24 +0800

    feat: only enable `prettier/prettier` rule for `eslint-plugin-prettier` `<5.1.2`

--- packages/eslint-plugin-mdx/src/configs/recommended.ts
+++ packages/eslint-plugin-mdx/src/configs/recommended.ts
@@ -1,4 +1,6 @@
-import type { Linter } from 'eslint'
+import { createRequire } from 'node:module'
+
+import type { ESLint, Linter } from 'eslint'
 
 import { base } from './base'
 
@@ -18,31 +20,59 @@ export const recommended: Linter.Config = {
   overrides,
 }
 
-try {
-  require.resolve('prettier')
-  require.resolve('eslint-plugin-prettier')
-  overrides.push(
-    {
-      files: '*.md',
-      rules: {
-        'prettier/prettier': [
-          'error',
-          {
-            parser: 'markdown',
-          },
-        ],
+// hack to bypass syntax error for commonjs
+const getImportMetaUrl = () =>
+  // eslint-disable-next-line @typescript-eslint/no-implied-eval, no-new-func
+  new Function('return import.meta.url')() as string
+
+const cjsRequire =
+  typeof require === 'undefined' ? createRequire(getImportMetaUrl()) : require
+
+const addPrettierRules = () => {
+  try {
+    cjsRequire.resolve('prettier')
+
+    const { meta } = cjsRequire('eslint-plugin-prettier') as ESLint.Plugin
+    const version = meta?.version || ''
+
+    /**
+     * @see https://github.com/prettier/eslint-plugin-prettier/releases/tag/v5.1.2
+     */
+    const [major, minor, patch] = version.split('.')
+
+    if (
+      +major > 5 ||
+      (+major === 5 &&
+        (+minor > 1 || (+minor === 1 && Number.parseInt(patch) >= 2)))
+    ) {
+      return
+    }
+
+    overrides.push(
+      {
+        files: '*.md',
+        rules: {
+          'prettier/prettier': [
+            'error',
+            {
+              parser: 'markdown',
+            },
+          ],
+        },
       },
-    },
-    {
-      files: '*.mdx',
-      rules: {
-        'prettier/prettier': [
-          'error',
-          {
-            parser: 'mdx',
-          },
-        ],
+      {
+        files: '*.mdx',
+        rules: {
+          'prettier/prettier': [
+            'error',
+            {
+              parser: 'mdx',
+            },
+          ],
+        },
       },
-    },
-  )
-} catch {}
+    )
+  } catch {}
+}
+
+addPrettierRules()
