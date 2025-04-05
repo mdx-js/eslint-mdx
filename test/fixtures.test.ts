e0fc29c36921ed601eca586ed617a097a575e3b9
JounQin <admin@1stg.me>
2025-04-05 11:56:21 +0800

    chore: workaround `prettier` related issue

--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -57,6 +57,9 @@ const ONE_MINUTE = 60_000
 
 const fixturesDir = path.resolve('test/fixtures')
 
+const relative = (filePath: string) =>
+  path.relative(fixturesDir, filePath).replaceAll('\\', '/')
+
 describe('fixtures', () => {
   it(
     'should match all snapshots',
@@ -64,13 +67,13 @@ describe('fixtures', () => {
       const patterns = ['test/fixtures/*', 'test/fixtures/**/*{md,mdx}']
       let results = await getCli().lintFiles(patterns)
       for (const { filePath, messages } of results) {
-        expect(messages).toMatchSnapshot(path.relative(fixturesDir, filePath))
+        expect(messages).toMatchSnapshot(relative(filePath))
       }
       results = await getCli(false, true).lintFiles(patterns)
       for (const { filePath, source, output } of results) {
         if (output != null && source !== output) {
           // eslint-disable-next-line jest/no-conditional-expect
-          expect(output).toMatchSnapshot(path.relative(fixturesDir, filePath))
+          expect(output).toMatchSnapshot(relative(filePath))
         }
       }
     },
@@ -84,13 +87,13 @@ describe('fixtures', () => {
         const patterns = 'test/fixtures/**/code-blocks.{md,mdx}'
         let results = await getCli(true).lintFiles(patterns)
         for (const { filePath, messages } of results) {
-          expect(messages).toMatchSnapshot(path.relative(fixturesDir, filePath))
+          expect(messages).toMatchSnapshot(relative(filePath))
         }
         results = await getCli(true, true).lintFiles(patterns)
         for (const { filePath, source, output } of results) {
           if (output != null && source !== output) {
             // eslint-disable-next-line jest/no-conditional-expect
-            expect(output).toMatchSnapshot(path.relative(fixturesDir, filePath))
+            expect(output).toMatchSnapshot(relative(filePath))
           }
         }
       },
