b670ae4b6b8e509d278ab3a90834231666b98609
JounQin <admin@1stg.me>
2025-05-16 14:44:33 +0800

    fix: take `cwd` into cache key account (#577)

---
+++ .changeset/grumpy-ads-play.md
@@ -0,0 +1,5 @@
+---
+"eslint-mdx": patch
+---
+
+fix: take `cwd` into cache key account
--- packages/eslint-plugin-mdx/src/helpers.ts
+++ packages/eslint-plugin-mdx/src/helpers.ts
@@ -22,7 +22,9 @@ export const getGlobals = <
     initialGlobals as R,
   )
 
+const importMetaUrl = import.meta.url
+
 /* istanbul ignore next */
-export const cjsRequire: CjsRequire = import.meta.url
-  ? createRequire(import.meta.url)
+export const cjsRequire: CjsRequire = importMetaUrl
+  ? createRequire(importMetaUrl)
   : require
